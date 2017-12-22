268b066f51c1cc32047e48fd185b9d1de9d33778
Deric Walintukan <deric.walintukan@gmail.com>
2017-12-22 12:18:44 +0700

    Add logic for searchlogoutput

--- src/formatter.js
+++ src/formatter.js
@@ -5,37 +5,27 @@ const Utils = require('./utils');
 class Formatter {
 
 	/**
-	 * Formats the output of searchlog by decoding eventName, indexed and unindexed params
-	 * @param  {object} rawOutput   Raw seachlog output
-	 * @param  {object} contractABI
-	 * @return {object}             Decoded searchlog output
-	 */
-  static searchLogOutput(rawOutput, contractABI) {
-
-    if (_.isUndefined(contractABI)) {
-      throw new Error(`contractABI is undefined.`);
-    }
-
-    // logDecoder is function created by EthjsAbi with ABI object
-    const logDecoder = EthjsAbi.logDecoder(contractABI);
-
-    return _.map(rawOutput, (resultEntry) => {
-
+   * Formats the output of searchlog by decoding eventName, indexed and unindexed params
+   * @param  {object} rawOutput   Raw seachlog output
+   * @return {object}             Decoded searchlog output
+   */
+  static searchLogOutput(rawOutput) {
+    return _.map(rawOutput, (resultEntry) => {  
       let formatted = _.assign({}, resultEntry);
 
-      // TODO: Format general fields of searchlogs
-
-      // Format 'log' field of searchlogs
-      // Each field of log needs to appended with '0x' to be parsed
       if (!_.isEmpty(resultEntry.log)) {
+        _.each(resultEntry.log, (item, index) => {
+          const topicHash = item.topics[0];
+          const eventTopicObj = _.find(EventTopics, { eventHash: topicHash });
 
-        const rawlogs = _.map(resultEntry.log, (logEntry) => ({
-          address: Utils.formatHexStr(logEntry.address),
-          data: Utils.formatHexStr(logEntry.data),
-          topics: _.map(logEntry.topics, Utils.formatHexStr)
-        }));
+          // Each field of log needs to appended with '0x' to be parsed
+          item.address = Utils.formatHexStr(item.address);
+          item.data = Utils.formatHexStr(item.data);
+          item.topics = _.map(item.topics, Utils.formatHexStr);
 
-        formatted.log = logDecoder(rawlogs);
+          const methodAbi = _.find(eventTopicObj.abi, { name: eventTopicObj.name });
+          resultEntry.log[index] = EthjsAbi.decodeLogItem(methodAbi, item);
+        });
       }
 
       return formatted;
