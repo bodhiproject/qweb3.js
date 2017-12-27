ffa13ca8f0bf56747649b0e467fb465964a2de4c
Deric Walintukan <deric.walintukan@gmail.com>
2017-12-27 16:05:19 +0700

    Add call test

--- test/config/config.js
+++ test/config/config.js
@@ -1,5 +1,6 @@
 const Config = {
     QTUM_RPC_ADDRESS: 'http://bodhi:bodhi@localhost:13889',
+    SENDER_ADDRESS: 'qKjn4fStBaAtwGiwueJf9qFxgpbAvf1xAy',
 };
 
 export default Config;
--- test/contract.js
+++ test/contract.js
@@ -1,5 +1,6 @@
 import 'babel-polyfill';
 import { assert } from 'chai';
+const utf8 = require('utf8');
 
 import Config from './config/config';
 import ContractMetadata from './data/contract_metadata';
@@ -19,6 +20,23 @@ describe('Contract', function() {
       assert.equal(contract.parent, qweb3);
       assert.equal(contract.address, ContractMetadata.EventFactory.address);
       assert.equal(contract.abi, ContractMetadata.EventFactory.abi);
+    });
+
+    it('removes the hex prefix from the address', async function() {
+      contract = new qweb3.Contract('0x1234567890', ContractMetadata.EventFactory.abi);
+      assert.equal(contract.address, '1234567890');
+    });
+  });
+
+  describe('call()', function() {
+    it('returns the values', async function() {
+      contract = new qweb3.Contract('dacd16bde8ff9f7689cb8d3363324c77fbb80950', ContractMetadata.TopicEvent.abi);
+      
+      var res = await contract.call('getEventName', { 
+        methodArgs: [], 
+        senderAddress: Config.SENDER_ADDRESS 
+      });
+      assert.equal(res[0].replace(/\0/g, ''), 'Who will win the 2018 NBA Finals Championships?');
     });
   });
 });
