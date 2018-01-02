3b10a5167a0c189d68d42e7a1c4a6a8cfa9462c4
Deric Walintukan <deric.walintukan@gmail.com>
2018-01-02 17:32:43 +0700

    Rearrange based on cli groupings

--- src/qweb3.js
+++ src/qweb3.js
@@ -9,6 +9,7 @@ class Qweb3 {
     this.provider = new HttpProvider(url);
   }
 
+  /********** MISC **********/
   /**
    * Returns true if getinfo request returns result; otherwise false
    * @return {Boolean}
@@ -19,30 +20,7 @@ class Qweb3 {
     }).then(res => Promise.resolve(!!res), err => Promise.resolve(false));
   }
 
-  /**
-   * Get the hex address of a Qtum address.
-   * @param {address} Qtum address
-   * @return {Promise} Hex string of the converted address or Error
-   */
-  getHexAddress(address) {
-    return this.provider.request({
-      method: 'gethexaddress',
-      params: [address],
-    });
-  }
-
-  /**
-   * Converts a hex address to qtum address.
-   * @param {hexAddress} Qtum address in hex format.
-   * @return {Promise} Qtum address or Error.
-   */
-  fromHexAddress(hexAddress) {
-    return this.provider.request({
-      method: 'fromhexaddress',
-      params: [hexAddress],
-    });
-  }
-
+  /********** BLOCKCHAIN **********/
   /*
   * @dev Returns the current block height that is synced with the client.
   * @return {Promise} Current block count or Error.
@@ -50,51 +28,6 @@ class Qweb3 {
   getBlockCount() {
     return this.provider.request({
       method: 'getblockcount',
-    });
-  }
-
-  /**
-   * Get transaction details by txid
-   * @param  {string} txid transaction Id (64 digits hexString),
-   *                       e.g. dfafd59050fbe825d884b1e9279924f42bfa9506ca11e3d1910141054858f338
-   * @return {Promise}     Promise containing result object or Error
-   */
-  getTransaction(txid) {
-    return this.provider.request({
-      method: 'gettransaction',
-      params: [txid],
-    });
-  }
-
-  /*
-  * @dev Returns the transaction receipt given the txid.
-  * @return {Promise} Transaction receipt or Error.
-  */
-  getTransactionReceipt(txid) {
-    return this.provider.request({
-      method: 'gettransactionreceipt',
-      params: [txid],
-    });
-  }
-
-  /*
-  * Lists unspent transaction outputs.
-  * @return {Promise} Array of unspent transaction outputs or Error
-  */
-  listUnspent() {
-    return this.provider.request({
-      method: 'listunspent',
-    });
-  }
-
-  /*
-  * Gets the Qtum address based on the account name.
-  * @return {Promise} Qtum address or Error
-  */
-  getAccountAddress(acctName) {
-    return this.provider.request({
-      method: 'getaccountaddress',
-      params: [acctName],
     });
   }
 
@@ -146,6 +79,77 @@ class Qweb3 {
 
     return this.provider.request(options)
       .then((results) => Formatter.searchLogOutput(results, contractMetadata, removeHexPrefix));
+  }
+
+  /********** RAW TRANSACTIONS **********/
+  /**
+   * Get the hex address of a Qtum address.
+   * @param {address} Qtum address
+   * @return {Promise} Hex string of the converted address or Error
+   */
+  getHexAddress(address) {
+    return this.provider.request({
+      method: 'gethexaddress',
+      params: [address],
+    });
+  }
+
+  /**
+   * Converts a hex address to qtum address.
+   * @param {hexAddress} Qtum address in hex format.
+   * @return {Promise} Qtum address or Error.
+   */
+  fromHexAddress(hexAddress) {
+    return this.provider.request({
+      method: 'fromhexaddress',
+      params: [hexAddress],
+    });
+  }
+
+  /********** WALLET **********/
+  /*
+  * Gets the Qtum address based on the account name.
+  * @return {Promise} Qtum address or Error
+  */
+  getAccountAddress(acctName) {
+    return this.provider.request({
+      method: 'getaccountaddress',
+      params: [acctName],
+    });
+  }
+  
+  /**
+   * Get transaction details by txid
+   * @param  {string} txid transaction Id (64 digits hexString),
+   *                       e.g. dfafd59050fbe825d884b1e9279924f42bfa9506ca11e3d1910141054858f338
+   * @return {Promise}     Promise containing result object or Error
+   */
+  getTransaction(txid) {
+    return this.provider.request({
+      method: 'gettransaction',
+      params: [txid],
+    });
+  }
+
+  /*
+  * @dev Returns the transaction receipt given the txid.
+  * @return {Promise} Transaction receipt or Error.
+  */
+  getTransactionReceipt(txid) {
+    return this.provider.request({
+      method: 'gettransactionreceipt',
+      params: [txid],
+    });
+  }
+
+  /*
+  * Lists unspent transaction outputs.
+  * @return {Promise} Array of unspent transaction outputs or Error
+  */
+  listUnspent() {
+    return this.provider.request({
+      method: 'listunspent',
+    });
   }
 }
 
