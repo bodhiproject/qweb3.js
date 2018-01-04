2321c585e3fea8bfd6e5d657f9a6dd5bf5ed2b52
Deric Walintukan <deric.walintukan@gmail.com>
2018-01-04 12:36:28 +0700

    Export all classes in index.js

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,24 @@
 'use strict';
 
-var Qweb3 = require('./qweb3');
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Contract = exports.Qweb3 = undefined;
+
+var _qweb = require('./qweb3');
+
+var _qweb2 = _interopRequireDefault(_qweb);
+
+var _contract = require('./contract');
+
+var _contract2 = _interopRequireDefault(_contract);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // dont override global variable
 if (typeof window !== 'undefined' && typeof window.Qweb3 === 'undefined') {
-    window.Qweb3 = Qweb3;
+  window.Qweb3 = _qweb2.default;
 }
 
-module.exports = Qweb3;
+exports.Qweb3 = _qweb2.default;
+exports.Contract = _contract2.default;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-var Qweb3 = require('./qweb3');
+import Qweb3 from './qweb3';
+import Contract from './contract';
 
 // dont override global variable
 if (typeof window !== 'undefined' && typeof window.Qweb3 === 'undefined') {
-    window.Qweb3 = Qweb3;
+  window.Qweb3 = Qweb3;
 }
 
-module.exports = Qweb3;
+export {
+  Qweb3,
+  Contract
+};
