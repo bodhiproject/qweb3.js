dbbba64f08375302902339b2d9cc4573f02d68c7
Deric Walintukan <deric.walintukan@gmail.com>
2018-02-13 17:07:26 +0700

    Rearrange constants

--- src/constants.js
+++ src/constants.js
@@ -6,14 +6,14 @@ module.exports = {
   STRING: 'string',
 
   // Regex
-  REGEX_STATIC_ARRAY: /\w+\[\d+\]/,
-  REGEX_STATIC_ADDRESS_ARRAY: /address\[\d+\]/,
-  REGEX_STATIC_BOOL_ARRAY: /bool\[\d+\]/,
   REGEX_UINT: /^uint\d+/,
   REGEX_INT: /^int\d+/,
   REGEX_BYTES: /bytes([1-9]|[12]\d|3[0-2])$/,
-  REGEX_STATIC_BYTES_ARRAY: /bytes([1-9]|[12]\d|3[0-2])(\[[0-9]+\])$/,
   REGEX_NUMBER: /[0-9]+/g,
+  REGEX_STATIC_ARRAY: /\w+\[\d+\]/,
+  REGEX_STATIC_ADDRESS_ARRAY: /address\[\d+\]/,
+  REGEX_STATIC_BOOL_ARRAY: /bool\[\d+\]/,
+  REGEX_STATIC_BYTES_ARRAY: /bytes([1-9]|[12]\d|3[0-2])(\[[0-9]+\])$/,
   REGEX_DYNAMIC_ARRAY: /\w+\[\]/,
 
   // Misc
