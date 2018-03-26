e6d75526aba8310d532be0a62c80d8fbbe86ac8c
Deric Walintukan <deric.walintukan@gmail.com>
2018-03-26 13:00:08 +0700

    Add list addr groupings function

--- src/qweb3.js
+++ src/qweb3.js
@@ -338,6 +338,17 @@ class Qweb3 {
   }
 
   /*
+  * Lists groups of addresses which have had their common ownership made public by common use as inputs
+  *   or as the resulting change in past transactions.
+  * @return {Promise} Array of addresses with QTUM balances or Error.
+  */
+  listAddressGroupings() {
+    return this.provider.request({
+      method: 'listaddressgroupings',
+    });
+  }
+
+  /*
   * Lists temporary unspendable outputs.
   * @return {Promise} Array of unspendable outputs or Error
   */
