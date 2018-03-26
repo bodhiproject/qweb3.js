c8cc81cddfc415637d4a1c88a4ed2ab99e6ce422
Deric Walintukan <deric.walintukan@gmail.com>
2018-03-26 13:21:23 +0700

    Fix qassert test

--- test/utils/qassert.js
+++ test/utils/qassert.js
@@ -1,10 +1,13 @@
 const _ = require('lodash');
+const chai = require('chai');
+
+const assert = chai.assert;
 
 module.exports = class QAssert {
 
   static isQtumAddress(address) {
     assert.isDefined(address);
-    assert.isEqual(_.size(address), 34);
+    assert.equal(_.size(address), 34);
     assert.isTrue(address.startsWith('q') || address.startsWith('Q'));
   }
 };
