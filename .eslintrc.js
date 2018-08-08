5cd1b82b88b57215cf8905ab30b50272903cf2e7
Deric Walintukan <deric.walintukan@gmail.com>
2018-08-08 10:12:09 +0700

    Remove commented rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,36 +17,5 @@ module.exports = {
     "max-len": [2, { "code": 120 }],
     "no-console": 0,
     "no-plusplus": 0
-    // "consistent-return": 0,
-    // "import/no-dynamic-require": 0,
-    // "import/prefer-default-export": 0,
-    // "no-use-before-define": ["error",
-    //   {
-    //     "functions": true,
-    //     "classes": false
-    //   }
-    // ],
-    // "no-param-reassign": ["error",
-    //   {
-    //     "props": true,
-    //     "ignorePropertyModificationsFor": ["poolAction"]
-    //   }
-    // ],
-    // "no-unused-vars": ["error",
-    //   {
-    //     "args": "none", // "all" for everything
-    //     "caughtErrors": "none"
-    //   }
-    // ],
-    // "no-param-reassign": ["error",
-    //   {
-    //     "props": false
-    //   }
-    // ],
-    // "object-property-newline": ["error",
-    //   {
-    //     "allowMultiplePropertiesPerLine": false
-    //   }
-    // ]
   }
 };
