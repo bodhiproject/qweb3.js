3a2a9f5b9e9d7482a97b3f305408c39f023dbd42
Deric Walintukan <deric.walintukan@gmail.com>
2018-08-13 17:28:16 +0700

    Remove contract and http provider exports

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 const Qweb3 = require('./qweb3');
-const Contract = require('./contract');
 const Encoder = require('./formatters/encoder');
 const Decoder = require('./formatters/decoder');
 const Utils = require('./utils');
-const HttpProvider = require('./providers/http-provider');
 
-// dont override global variable
+// Attach Qweb3 to window
 if (typeof window !== 'undefined' && typeof window.Qweb3 === 'undefined') {
   window.Qweb3 = Qweb3;
 }
 
 module.exports = {
   Qweb3,
-  Contract,
   Encoder,
   Decoder,
   Utils,
-  HttpProvider,
 };
