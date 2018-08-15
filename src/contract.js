02fde1ee61bce446ee9a9b8929739b9f24264f1c
Deric Walintukan <deric.walintukan@gmail.com>
2018-08-15 14:22:26 +0700

    Use new encoder funcs

--- src/contract.js
+++ src/contract.js
@@ -1,5 +1,6 @@
 const { initProvider } = require('./providers');
 const Utils = require('./utils');
+const Encoder = require('./formatters/encoder');
 const Decoder = require('./formatters/decoder');
 
 const DEFAULT_AMOUNT = 0;
@@ -20,58 +21,51 @@ class Contract {
   }
 
   /**
-   * @dev Executes a callcontract on a view/pure method via the qtum-cli.
+   * Executes a callcontract on a view/pure method.
    * @param {string} methodName Name of contract method
-   * @param {array} params Parameters of contract method
-   * @return {Promise} Promise containing result object or Error
+   * @param {object} params Parameters of contract method
+   * @return {Promise} Call result.
    */
   async call(methodName, params) {
     const { methodArgs, senderAddress } = params;
-    const { method: methodObj, args } = this.validateMethodAndArgs(methodName, methodArgs);
-
-    let result = await this.provider.rawCall('callcontract', [
-      this.address,
-      this.constructDataHex(methodObj, args),
-      senderAddress,
-    ]);
-    // Format the result
-    result = Decoder.decodeCall(result, this.abi, methodName, true);
+    const data = Encoder.constructData(this.abi, methodName, methodArgs);
+    let result = await this.provider.rawCall('callcontract', [this.address, data, senderAddress]);
+    result = Decoder.decodeCall(result, this.abi, methodName, true); // Format the result
     return result;
   }
 
-  /*
-  * @dev Executes a sendtocontract on this contract via the qtum-cli.
-  * @param methodName Method name to execute as a string.
-  * @param params Parameters of the contract method.
-  * @return The transaction id of the sendtocontract.
-  */
+  /**
+   * Executes a sendtocontract transaction.
+   * @param {string} methodName Method name to call.
+   * @param {object} params Parameters of the contract method.
+   * @return {Promise} Transaction ID of the sendtocontract.
+   */
   async send(methodName, params) {
     // Throw if methodArgs or senderAddress is not defined in params
     Utils.paramsCheck('send', params, ['methodArgs', 'senderAddress']);
 
     const { methodArgs, amount, gasLimit, gasPrice, senderAddress } = params;
-    const { method: methodObj, args } = this.validateMethodAndArgs(methodName, methodArgs);
+    const data = Encoder.constructData(this.abi, methodName, methodArgs);
     const amt = amount || DEFAULT_AMOUNT;
     const limit = gasLimit || DEFAULT_GAS_LIMIT;
     const price = gasPrice || DEFAULT_GAS_PRICE;
 
     const result = await this.provider.rawCall('sendtocontract', [
       this.address,
-      this.constructDataHex(methodObj, args),
+      data,
       amt,
       limit,
       price.toFixed(8),
       senderAddress,
     ]);
 
-    // Add request object with params used for request
+    // Add original request params to result obj
     result.args = {
       contractAddress: this.address,
       amount: amt,
       gasLimit: limit,
       gasPrice: price,
     };
-
     return result;
   }
 }
