ba9c78ec32d11db4e5fdac2e09770e94f013663b
Deric Walintukan <deric.walintukan@gmail.com>
2018-08-15 14:56:37 +0700

    Remove auto injecting qweb3 into window

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ const Encoder = require('./formatters/encoder');
 const Decoder = require('./formatters/decoder');
 const Utils = require('./utils');
 
-// Attach Qweb3 to window
-if (typeof window !== 'undefined' && typeof window.Qweb3 === 'undefined') {
-  window.Qweb3 = Qweb3;
-}
-
 module.exports = {
   Qweb3,
   Encoder,
