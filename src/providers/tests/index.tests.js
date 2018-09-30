1457cbf141270528739e2f1b3be0e95dfea8224c
Deric Walintukan <deric.walintukan@gmail.com>
2018-09-30 15:30:41 +0700

    Change tests for init provider

--- src/providers/tests/index.tests.js
+++ src/providers/tests/index.tests.js
@@ -7,7 +7,7 @@ const Decoder = require('../../formatters/decoder');
 
 const { assert } = chai;
 
-describe('Providers', () => {
+describe.only('Providers', () => {
   describe('initProvider', () => {
     it('accepts a url string', () => {
       const instance = initProvider('http://bodhi:bodhi@localhost:13889');
@@ -21,7 +21,12 @@ describe('Providers', () => {
       assert.isTrue(instance.constructor.name === 'HttpProvider');
     });
 
+    it('throws if the provider is undefined', () => {
+      assert.throws(() => initProvider(undefined));
+    });
+
     it('throws if not a compatible provider', () => {
+      assert.throws(() => initProvider({ rawCall: 'test' }));
       assert.throws(() => initProvider(new Decoder()));
     });
   });
